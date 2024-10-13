602f3b00f08e3b230b535181f0228cdc15371528
unknown <ikhsann330@gmail.com>
2024-10-13 12:47:45 +0700

    yaya

---
+++ src/js/index.js
@@ -0,0 +1,61 @@
+const scrollable = document.getElementById('scrollable-content');
+const scrollLeftBtn = document.getElementById('scrollLeft');
+const scrollRightBtn = document.getElementById('scrollRight');
+
+scrollLeftBtn.addEventListener('click', () => {
+  scrollable.scrollBy({
+    left: -300,
+    behavior: 'smooth',
+  });
+});
+
+scrollRightBtn.addEventListener('click', () => {
+  scrollable.scrollBy({
+    left: 300,
+    behavior: 'smooth',
+  });
+});
+
+
+// navbar
+window.addEventListener('scroll', function () {
+  const navbar = document.getElementById('navbar');
+  const navbarTexts = document.querySelectorAll('.navbar-text');
+  if (window.scrollY > 50) {
+      navbar.classList.remove('navbar-large');
+      navbar.classList.add('navbar-small');
+      navbarTexts.forEach(text => text.classList.remove('navbar-text-large'));
+      navbarTexts.forEach(text => text.classList.add('navbar-text-small'));
+  } else {
+      navbar.classList.remove('navbar-small');
+      navbar.classList.add('navbar-large');
+      navbarTexts.forEach(text => text.classList.remove('navbar-text-small'));
+      navbarTexts.forEach(text => text.classList.add('navbar-text-large'));
+  }
+});
+
+// Script untuk mobile menu toggle
+const mobileMenuButton = document.querySelector('.mobile-menu-button');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+mobileMenuButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+  document.querySelector('.hamburger-icon').classList.toggle('hidden');
+  document.querySelector('.cross-icon').classList.toggle('hidden');
+});
+
+// Hasil
+
+function expandVideo(clickedVideo) {
+  const videoBoxes = document.querySelectorAll('.video-box');
+
+  videoBoxes.forEach(videoBox => {
+    // Reset all videos to their default size
+    videoBox.classList.remove('expanded');
+    videoBox.classList.add('shrink');
+  });
+
+  // Expand the clicked video
+  clickedVideo.classList.add('expanded');
+  clickedVideo.classList.remove('shrink');
+}
